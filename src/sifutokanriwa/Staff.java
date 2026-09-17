413134d5fdd8f5fa1955ed03fbc9d85ea3642c9d
hattori shoma <hattorishoma@hattorishomanonotobukkukonpyuta>
2026-09-17 10:06:53 +0900

    test

--- src/sifutokanriwa/Staff.java
+++ src/sifutokanriwa/Staff.java
@@ -2,4 +2,20 @@ package sifutokanriwa;
 
 public class Staff {
 
+	int stafID;
+	String name;
+	String roru;
+
+	public Staff(int staffID, String name, String roru) {
+		this.stafID = staffID;
+		this.name = name;
+		this.roru = roru;
+
+	}
+
+	public void displayInfo() {
+		System.out.println("スタッフID;" + stafID);
+		System.out.println("名前;" + name);
+		System.out.println("役割;" + roru);
+	}
 }
